77d6485fb53f8c172004b43ac838cac4e2d3f76f
ziko7110 <ziko7110@gmail.com>
2020-06-14 21:30:49 +0900

    ビュー作成

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,7 +1,53 @@
 @extends('layouts.app')
 @section('content')
 <body>
-    <div class = contents>
+    <div class="bs-docs-section">
+        <div class="row">
+            <div class="col-lg-12">
+            <div class="page-header">
+                <h1 id="tables">TODAY SCHEDULE</h1>
+            </div>
+            <div class="bs-component">
+                <table class="table table-hover">
+                <thead>
+                    <tr>
+                    <th scope="col"><h3><a class="drop" href="https://clubdrop.jp/">DROP</a></h3></th>
+                    <th scope="col"><h3><a class="beyond" href="https://beyond-osaka.jp/">BEYOND</a><h3></th>
+                    <th scope="col"><h3><a class="varon" href="https://osaka-varon.jp/">VARON</a></h3></th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr class="table-active">
+                    <th scope="row">スムージチークスワンマン 〜七夕リクエスト祭り〜</th>
+                    <td>COMING HOME Vol.40 - MODERN KNOCK "SQUARe:<br> tour 2020"&New Side Chapter"RevolC<br> (REM;EMBER me.)"Release Party in OSAKA</td>
+                    <td>WAGAMAMARAKIA 2MAN SHOWS「The Take Back」</td>
+                    <!-- <td>Column content</td> -->
+                    </tr>
+                    <tr>
+                    <th scope="row"><div>[ACT]</div>スムージチークス</th>
+                    <td><div>[ACT]</div>AFTER SQUALL <br> AX LITTLE CITY <br> MODERN KNOCK <br> New Side Chapter <br> Past life was Thief <br> SIGHTS</td>
+                    <td><div>[ACT]</div>我儘ラキア-BAND SET- /<br> FAITH /<br> Djつぶら</td>
+                    <!-- <td>Column content</td> -->
+                    </tr>
+                    <tr class="table-light">
+                    <th scope="row"><div>[OPEN/START]<div>18:30/19:00<div>[料金]</div>ADV/DOOR ￥3000/￥--(1D別)</th>
+                    <td><div>[OPEN/START]<div>18:30/19:00<div>[料金]</div>ADV/DOOR ￥3000/￥--(1D別)</td>
+                    <td><div>[OPEN/START]<div>18:30/19:00<div>[料金]</div>ADV/DOOR ￥2500/￥3200(1D別)</td>
+                    <!-- <td>[OPEN/START]18:30/19:00[料金]ADV/DOOR ￥3000/￥--(1D別)</td> -->
+                    </tr>
+                    <!-- <tr class="table-dark">
+                    <th scope="row">Dark</th>
+                    <td>Column content</td>
+                    <td>Column content</td>
+                    <td>Column content</td>
+                    </tr> -->
+                </tbody>
+                </table> 
+            </div><!-- /example -->
+            </div>
+        </div>
+        </div>
+    <!-- <div class = contents>
         <h2>TODAY SCHEDULE</h2>
         <div class = "livehouses">
             <div class = "livehouse_names">
@@ -43,12 +89,9 @@
                         <div class = "event1_title_name"></div>
                     </div>
                     <ul div class="varon_bands">
-
                         <li></li>
-
                     </ul>
                     <div class = "flyer">
-
                     </div>
                     <div class= "event3_price">
                         <p></p>
@@ -57,6 +100,6 @@
                 </div>
             </div>
         </div>
-    <div>
+    <div> -->
 </body>
 @endsection('content')
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -13,7 +13,8 @@
     <link href="{{ asset('/css/tasks.css') }}" rel="stylesheet">
 
     <!-- Styles -->
-    <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
+    <!-- <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" rel="stylesheet"> -->
     <script  src="https://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-cookie/1.4.1/jquery.cookie.js"></script>
     <script src="{{ asset('/js/test.js') }}"></script>
@@ -38,28 +39,55 @@
     <script src="/js/lightbox.js" type="text/javascript"></script>
     <!-- /Users/naitoukouzi/quickstart_2/public/js/lightbox.js -->
 </head>
-<body>
-    <!-- Google Tag Manager (noscript) -->
-    <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-TM9JR5X"
-    height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
-    <!-- End Google Tag Manager (noscript) -->
-<header>
-<header>
-        <h1>EVERYWHEEL<span>2019</span></h1>
-        <div class="nav">
-            <a class="inquiry" href=#>問い合わせ</a>
-                <a class="inquiry2" href= '/users/sign_out'>ログアウト</a>
-            <a class="inquiry2" href= '/users/sign_in'>管理者用ページ</a>
-        </div>
-    </header>
-<headr>
+<nav class="navbar navbar-expand-md navbar-dark bg-dark">
+    <!-- <a class="navbar-brand" href="#">Expand at md</a> -->
+    <a class="navbar-brand" href="#">EVERYWHEEL<span>2020</span></a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarsExample04" aria-controls="navbarsExample04" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+    </button>
+
+    <div class="collapse navbar-collapse" id="navbarsExample04">
+        <ul class="navbar-nav mr-auto">
+        <li class="nav-item active">
+            <a class="nav-link" href="#">ホーム <span class="sr-only">（現在位置）</span></a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" href="#">リンク</a>
+        </li>
+        <!-- <li class="nav-item">
+            <a class="nav-link disabled" href="#">無効なアイテム</a>
+        </li> -->
+        <li class="nav-item dropdown">
+            <a class="nav-link dropdown-toggle" href="https://example.com" id="dropdown04" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">ドロップダウン</a>
+            <div class="dropdown-menu" aria-labelledby="dropdown04">
+            <a class="dropdown-item" href="#">問い合わせ</a>
+            <a class="dropdown-item" href="#">ログアウト</a>
+            <a class="dropdown-item" href="#">管理者用ページ</a>
+            </div>
+        </li>
+        </ul>
+        <form class="form-inline my-2 my-md-0">
+        <input class="form-control" type="text" placeholder="検索">
+        </form>
+    </div>
+</nav>
 @yield('content')
-<footer>
-        <div class="footer_content">
-            <p class="footer_copy">Copyright(C) everywheel. All Rights Researved.</p>
-        </div>
-    </footer>
+<!-- Footer -->
+<footer class="page-footer font-small special-color-dark pt-4">
+    <div class="container">
+    </div>
+  <!-- Copyright -->
+  <div class="footer-copyright text-center py-3">Copyright(C) everywheel. All Rights Researved.</div>
+</footer>
+<!-- <footer>
+    <div class="footer_content">
+        <p class="footer_copy">Copyright(C) everywheel. All Rights Researved.</p>
+    </div>
+</footer> -->
 <!-- JavaScripts -->
-<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>  
+<!-- <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>   -->
+<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js" integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI" crossorigin="anonymous"></script>
 </body>
 </html>
