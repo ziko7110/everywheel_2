ae1a7b8c0d69e52381b9fc64bf70712f451df795
ziko7110 <ziko7110@gmail.com>
2020-06-30 08:15:07 +0900

    不要行削除

--- app/Http/Controllers/LiveController.php
+++ app/Http/Controllers/LiveController.php
@@ -12,37 +12,29 @@ class LiveController extends Controller
     {
          // DROPをスクレイピング
         $goutte = GoutteFacade::request('GET', 'https://clubdrop.jp/schedule/detail/8908');
-         //テキストを取得するための配列を準備
         $drop_artists = array();
-         //テキストを取得
         $goutte->filter('.other_artists')->each(function ($node) use (&$drop_artists) {
             $drop_artists[] = $node->text();
         });
 
         // BEYONDをスクレイピング
         $goutte = GoutteFacade::request('GET', 'https://beyond-osaka.jp/schedule/detail/8851');
-        //テキストを取得するための配列を準備
         $beyond_artists = array();
-        //テキストを取得
         $goutte->filter('.other_artists')->each(function ($node) use (&$beyond_artists) {
             $beyond_artists[] = $node->text();
         });
 
         // ここではVaronをスクレイピング
         $goutte = GoutteFacade::request('GET', 'https://osaka-varon.jp/schedule/detail/3332');
-        //テキストを取得するための配列を準備
         $varon_artists = array();
-        //テキストを取得
         $goutte->filter('.other_artists')->each(function ($node) use (&$varon_artists) {
             $varon_artists[] = $node->text();
         });
         $params = [
-            // 'images' => $images,
             'varon_artists' => $varon_artists,
             'beyond_artists' => $beyond_artists,
             'drop_artists' => $drop_artists,
         ];
-// eval(\Psy\sh());
         return view('index', $params);
     }
 
