300c2ce4d563aa16cbef4fdc72f350309860204f
ziko7110 <ziko7110@gmail.com>
2020-07-08 08:59:57 +0900

    インデント修正

--- config/broadcasting.php
+++ config/broadcasting.php
@@ -31,27 +31,27 @@ return [
     'connections' => [
 
         'pusher' => [
-            'driver' => 'pusher',
-            'key' => env('PUSHER_APP_KEY'),
-            'secret' => env('PUSHER_APP_SECRET'),
-            'app_id' => env('PUSHER_APP_ID'),
-            'options' => [
-                'cluster' => env('PUSHER_APP_CLUSTER'),
+            'driver'    => 'pusher',
+            'key'       => env('PUSHER_APP_KEY'),
+            'secret'    => env('PUSHER_APP_SECRET'),
+            'app_id'    => env('PUSHER_APP_ID'),
+            'options'   => [
+                'cluster'   => env('PUSHER_APP_CLUSTER'),
                 'encrypted' => true,
             ],
         ],
 
         'redis' => [
-            'driver' => 'redis',
-            'connection' => 'default',
+            'driver'        => 'redis',
+            'connection'    => 'default',
         ],
 
         'log' => [
-            'driver' => 'log',
+            'driver'    => 'log',
         ],
 
         'null' => [
-            'driver' => 'null',
+            'driver'    => 'null',
         ],
 
     ],
