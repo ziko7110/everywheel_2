f6bf98c5a8e52a70e82d1749df53c2ef3e0696f8
ziko7110 <ziko7110@gmail.com>
2020-07-09 07:22:37 +0900

    Update logging.php

--- config/logging.php
+++ config/logging.php
@@ -34,47 +34,47 @@ return [
 
     'channels' => [
         'stack' => [
-            'driver' => 'stack',
-            'channels' => ['single'],
+            'driver'    => 'stack',
+            'channels'  => ['single'],
         ],
 
         'single' => [
-            'driver' => 'single',
-            'path' => storage_path('logs/laravel.log'),
-            'level' => 'debug',
+            'driver'    => 'single',
+            'path'      => storage_path('logs/laravel.log'),
+            'level'     => 'debug',
         ],
 
         'daily' => [
-            'driver' => 'daily',
-            'path' => storage_path('logs/laravel.log'),
-            'level' => 'debug',
-            'days' => 7,
+            'driver'    => 'daily',
+            'path'      => storage_path('logs/laravel.log'),
+            'level'     => 'debug',
+            'days'      => 7,
         ],
 
         'slack' => [
-            'driver' => 'slack',
-            'url' => env('LOG_SLACK_WEBHOOK_URL'),
-            'username' => 'Laravel Log',
-            'emoji' => ':boom:',
-            'level' => 'critical',
+            'driver'    => 'slack',
+            'url'       => env('LOG_SLACK_WEBHOOK_URL'),
+            'username'  => 'Laravel Log',
+            'emoji'     => ':boom:',
+            'level'     => 'critical',
         ],
 
         'stderr' => [
-            'driver' => 'monolog',
-            'handler' => StreamHandler::class,
-            'with' => [
-                'stream' => 'php://stderr',
+            'driver'    => 'monolog',
+            'handler'   => StreamHandler::class,
+            'with'      => [
+                'stream'    => 'php://stderr',
             ],
         ],
 
         'syslog' => [
-            'driver' => 'syslog',
-            'level' => 'debug',
+            'driver'    => 'syslog',
+            'level'     => 'debug',
         ],
 
         'errorlog' => [
-            'driver' => 'errorlog',
-            'level' => 'debug',
+            'driver'    => 'errorlog',
+            'level'     => 'debug',
         ],
     ],
 
