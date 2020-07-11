a846c37e9865e4e2ceba8977fa5119e565fc0377
ziko7110 <ziko7110@gmail.com>
2020-07-11 09:08:24 +0900

    Update services.php

--- config/services.php
+++ config/services.php
@@ -20,19 +20,19 @@ return [
     ],
 
     'ses' => [
-        'key' => env('SES_KEY'),
-        'secret' => env('SES_SECRET'),
-        'region' => env('SES_REGION', 'us-east-1'),
+        'key'       => env('SES_KEY'),
+        'secret'    => env('SES_SECRET'),
+        'region'    => env('SES_REGION', 'us-east-1'),
     ],
 
     'sparkpost' => [
-        'secret' => env('SPARKPOST_SECRET'),
+        'secret'    => env('SPARKPOST_SECRET'),
     ],
 
     'stripe' => [
-        'model' => App\User::class,
-        'key' => env('STRIPE_KEY'),
-        'secret' => env('STRIPE_SECRET'),
+        'model'     => App\User::class,
+        'key'       => env('STRIPE_KEY'),
+        'secret'    => env('STRIPE_SECRET'),
     ],
 
 ];
